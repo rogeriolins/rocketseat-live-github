521b01cea529fa16d19f584dfe1b166a81a2bd2d
Rogerio Lins <rogerio@prologic.com.br>
2019-10-18 17:44:20 -0300

    chore: alteracao msg retorno

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/test',(req,res) => {
-    return res.json({hello: "word"});
+    return res.json({hello: "word 2"});
 })
 
 app.listen(3333);
